f31cf6b7cdd5ef3e2a1605db0b79c546c5a6dcb4
fmacicasan <flo.macicasan@gmail.com>
2013-05-13 10:32:02 +0300

    Adding mocked test 2

---
+++ src/test/java/MockTest2.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: fmacicasan
+ * Date: 5/13/13
+ * Time: 10:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MockTest2 {
+}
